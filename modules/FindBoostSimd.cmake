fa5079a3387024b0a063253e8dd65f300246344c
André Tupinambá <andrelrt@gmail.com>
2017-02-05 11:00:01 +0000

    Get FindBoostSimd a bit better

--- modules/FindBoostSimd.cmake
+++ modules/FindBoostSimd.cmake
@@ -1,20 +1,20 @@
 # - Find Boost.SIMD
 # Find the native Boost.SIMD includes
 #
-#  BOOST_SIMD_INCLUDE_DIR    - where to find fftw3.h
-#  BOOST_SIMD_FOUND       - True if Boost.SIMD found.
+#  BOOST_SIMD_INCLUDE_DIR    - where to find boost/simd/pack.hpp
+#  BOOST_SIMD_FOUND          - True if Boost.SIMD found.
 
 if (BOOST_SIMD_INCLUDE_DIR)
   # Already in cache, be silent
   set (BOOST_SIMD_FIND_QUIETLY TRUE)
 endif (BOOST_SIMD_INCLUDE_DIR)
 
-set (BOOST_SIMD_HINT $ENV{BOOST_SIMD_ROOT}/include)
+if (NOT "$ENV{BOOST_SIMD_ROOT}" STREQUAL "")
+  set (BOOST_SIMD_HINT $ENV{BOOST_SIMD_ROOT}/include)
+endif()
 
 find_path (BOOST_SIMD_INCLUDE_DIR boost/simd/pack.hpp HINTS ${BOOST_SIMD_HINT})
 
-# handle the QUIETLY and REQUIRED arguments and set BOOST_SIMD_FOUND to TRUE if
-# all listed variables are TRUE
 include (FindPackageHandleStandardArgs)
 find_package_handle_standard_args (Boost.SIMD DEFAULT_MSG BOOST_SIMD_INCLUDE_DIR)
 
